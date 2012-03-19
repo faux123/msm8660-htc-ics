2fae362efedb2b95b652bb7e2bbfca034c24cbc2
faux123 <reioux@gmail.com>
2012-03-19 15:47:43 -0700

    compilation: fix merge error net/ipv4/xfrm4_mode_tunnel.c

--- net/ipv4/xfrm4_mode_tunnel.c
+++ net/ipv4/xfrm4_mode_tunnel.c
@@ -83,13 +83,6 @@ static int xfrm4_mode_tunnel_input(struct xfrm_state *x, struct sk_buff *skb)
 	if (!(x->props.flags & XFRM_STATE_NOECN))
 		ipip_ecn_decapsulate(skb);
 
-	old_mac = skb_mac_header(skb);
-	skb_set_mac_header(skb, -skb->mac_len);
-	if (old_mac != NULL) {
-		/*printk(KERN_ERR "[XFRM] old_mac!=NULL: %s\n", __func__);*/
-		memmove(skb_mac_header(skb), old_mac, skb->mac_len);
-	}
-
 	skb_reset_network_header(skb);
 	skb_mac_header_rebuild(skb);
 
