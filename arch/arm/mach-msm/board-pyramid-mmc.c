0c8ab33641a0cc2161794835432eb23895507e9e
faux123 <reioux@gmail.com>
2012-03-20 11:05:26 -0700

    board: lowered BCM4329 power draw to 2.3 volts
    the voltage is based on BCM4329 datasheet of 2.3 volts minimum.

--- arch/arm/mach-msm/board-pyramid-mmc.c
+++ arch/arm/mach-msm/board-pyramid-mmc.c
@@ -154,7 +154,8 @@ static unsigned int pyramid_wifi_status(struct device *dev)
 
 static unsigned int pyramid_wifislot_type = MMC_TYPE_SDIO_WIFI;
 static struct mmc_platform_data pyramid_wifi_data = {
-        .ocr_mask               = MMC_VDD_28_29,
+	// according to BCM4329 datasheet, 2.3v is min
+        .ocr_mask               = MMC_VDD_22_23,
         .status                 = pyramid_wifi_status,
         .register_status_notify = pyramid_wifi_status_register,
         .embedded_sdio          = &pyramid_wifi_emb_data,
