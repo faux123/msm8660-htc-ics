4dc956341c483450a8eaf23168add20b48132f0d
Matt Filetto <matt.filetto@gmail.com>
2012-03-26 19:45:10 -0700

    disable mpdecision

--- releasetools/kernel/modclean.sh
+++ releasetools/kernel/modclean.sh
@@ -1,4 +1,6 @@
 #!/sbin/sh
 busybox rm /system/lib/modules/*
 busybox rm /system/etc/init.d/90zram
+busybox mv /system/bin/mpdecision /system/bin/mpdecision_dis
+busybox mv /system/bin/thermald /system/bin/thermald_dis
 return $?
