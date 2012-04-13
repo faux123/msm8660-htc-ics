e6ae871baaa1a3439bd174d17d7573f9e1d8afac
faux123 <reioux@gmail.com>
2012-04-13 09:49:27 -0700

    cpufreq: interactive: don't drop speed if recently at higher load
    Apply min_sample_time to the last time the current target speed
    was originally requested or re-validated as appropriate for the
    current load, not to the time since the current speed was
    originally set.  Avoids periodic dips in speed during bursty
    loads.
    
    Change-Id: I250bda657985de60373f9897cc41f480664d51a1
    Signed-off-by: Todd Poynor <toddpoynor@google.com>
    (cherry picked from commit e04e953)
    modified for MSM8660 by faux123

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -38,8 +38,8 @@ struct cpufreq_interactive_cpuinfo {
 	u64 idle_exit_time;
 	u64 timer_run_time;
 	int idling;
-	u64 freq_change_time;
-	u64 freq_change_time_in_idle;
+	u64 target_set_time;
+	u64 target_set_time_in_idle;
 	struct cpufreq_policy *policy;
 	struct cpufreq_frequency_table *freq_table;
 	unsigned int target_freq;
@@ -143,9 +143,9 @@ static void cpufreq_interactive_timer(unsigned long data)
 		cpu_load = 100 * (delta_time - delta_idle) / delta_time;
 
 	delta_idle = (unsigned int) cputime64_sub(now_idle,
-						pcpu->freq_change_time_in_idle);
+						pcpu->target_set_time_in_idle);
 	delta_time = (unsigned int) cputime64_sub(pcpu->timer_run_time,
-						  pcpu->freq_change_time);
+						  pcpu->target_set_time);
 
 	if ((delta_time == 0) || (delta_idle > delta_time))
 		load_since_change = 0;
@@ -184,17 +184,21 @@ static void cpufreq_interactive_timer(unsigned long data)
 
 	new_freq = pcpu->freq_table[index].frequency;
 
-	if (pcpu->target_freq == new_freq)
-		goto rearm_if_notmax;
-
 	/*
 	 * Do not scale down unless we have been at this frequency for the
 	 * minimum sample time.
 	 */
 	if (new_freq < pcpu->target_freq) {
-		if (cputime64_sub(pcpu->timer_run_time, pcpu->freq_change_time)
+		if (cputime64_sub(pcpu->timer_run_time, pcpu->target_set_time)
 		    < min_sample_time)
 			goto rearm;
+	}
+
+	pcpu->target_set_time_in_idle = now_idle;
+	pcpu->target_set_time = pcpu->timer_run_time;
+
+	if (pcpu->target_freq == new_freq) {
+		goto rearm_if_notmax;
 	}
 
 	if (new_freq < pcpu->target_freq) {
@@ -367,9 +371,6 @@ static int cpufreq_interactive_up_task(void *data)
 			__cpufreq_driver_target(pcpu->policy,
 						pcpu->target_freq,
 						CPUFREQ_RELATION_H);
-			pcpu->freq_change_time_in_idle =
-				get_cpu_idle_time_us(cpu,
-						     &pcpu->freq_change_time);
 		}
 	}
 
@@ -399,9 +400,6 @@ static void cpufreq_interactive_freq_down(struct work_struct *work)
 		__cpufreq_driver_target(pcpu->policy,
 					pcpu->target_freq,
 					CPUFREQ_RELATION_H);
-		pcpu->freq_change_time_in_idle =
-			get_cpu_idle_time_us(cpu,
-					     &pcpu->freq_change_time);
 	}
 }
 
@@ -529,9 +527,9 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 			pcpu->policy = policy;
 			pcpu->target_freq = policy->cur;
 			pcpu->freq_table = freq_table;
-			pcpu->freq_change_time_in_idle =
+			pcpu->target_set_time_in_idle =
 				get_cpu_idle_time_us(j,
-					     &pcpu->freq_change_time);
+					     &pcpu->target_set_time);
 			pcpu->governor_enabled = 1;
 			smp_wmb();
 		}
