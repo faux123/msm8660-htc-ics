9f48dc5ae37c8477b3cbf6d7ddfdd3310d6e2c82
faux123 <reioux@gmail.com>
2012-04-17 17:04:06 -0700

    msm: kgsl: Ensure glitch free changing of the GPU frequency
    If there are requests to skip levels while the clocks are on
    be sure to hit the intermediate frequencies.
    
    modified from CAF baseline for HTC GPU driver by faux123

--- drivers/gpu/msm_adreno220/kgsl_pwrctrl.c
+++ drivers/gpu/msm_adreno220/kgsl_pwrctrl.c
@@ -68,10 +68,20 @@ void kgsl_pwrctrl_pwrlevel_change(struct kgsl_device *device,
 		new_level >= pwr->thermal_pwrlevel &&
 		new_level != pwr->active_pwrlevel) {
 		struct kgsl_pwrlevel *pwrlevel = &pwr->pwrlevels[new_level];
+		int diff = new_level - pwr->active_pwrlevel;
+		int d = (diff > 0) ? 1 : -1;
+		int level = pwr->active_pwrlevel;
 		pwr->active_pwrlevel = new_level;
 		if ((test_bit(KGSL_PWRFLAGS_CLK_ON, &pwr->power_flags)) ||
 			(device->state == KGSL_STATE_NAP))
-			clk_set_rate(pwr->grp_clks[0], pwrlevel->gpu_freq);
+			/* Don't shift by more than one level at a time to
+			 * avoid glitches.
+			 */
+			while (level != new_level) {
+				level += d;
+				clk_set_rate(pwr->grp_clks[0],
+						pwr->pwrlevels[level].gpu_freq);
+			}
 		if (test_bit(KGSL_PWRFLAGS_AXI_ON, &pwr->power_flags)) {
 			if (pwr->pcl)
 				msm_bus_scale_client_update_request(pwr->pcl,
