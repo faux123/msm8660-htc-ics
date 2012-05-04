edad28152476943ac5b1aa953f0fda33969478af
faux123 <reioux@gmail.com>
2012-05-03 20:35:31 -0700

    android, lowmemorykiller: remove task handoff notifier
    The task handoff notifier leaks task_struct since it never gets freed
    after the callback returns NOTIFY_OK, which means it is responsible for
    doing so.
    
    It turns out the lowmemorykiller actually doesn't need this notifier at
    all.  It's used to prevent unnecessary killing by waiting for a thread
    to exit as a result of lowmem_shrink(), however, it's possible to do
    this in the same way the kernel oom killer works by setting TIF_MEMDIE
    and avoid killing if we're still waiting for it to exit.
    
    The kernel oom killer will already automatically set TIF_MEMDIE for
    threads that are attempting to allocate memory that have a fatal signal.
    The thread selected by lowmem_shrink() will have such a signal after the
    lowmemorykiller sends it a SIGKILL, so this won't result in an
    unnecessary use of memory reserves for the thread to exit.
    
    This has the added benefit that we don't have to rely on
    CONFIG_PROFILING to prevent needlessly killing tasks.
    
    Reported-by: Werner Landgraf <w.landgraf@ru.ru>
    Cc: stable@vger.kernel.org
    Signed-off-by: David Rientjes <rientjes@google.com>
    Acked-by: Colin Cross <ccross@android.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    modified for HTC kernel from Linux 3.4+ by faux123

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -73,7 +73,6 @@ static size_t lowmem_minfile[6] = {
 };
 static int lowmem_minfile_size = 6;
 
-static struct task_struct *lowmem_deathpending;
 static unsigned long lowmem_deathpending_timeout;
 static uint32_t lowmem_check_filepages = 0;
 
@@ -84,63 +83,6 @@ static uint32_t lowmem_check_filepages = 0;
 			printk(x);			\
 		}					\
 	} while (0)
-
-static int
-task_notify_func(struct notifier_block *self, unsigned long val, void *data);
-
-static struct notifier_block task_nb = {
-	.notifier_call	= task_notify_func,
-};
-
-static int
-task_notify_func(struct notifier_block *self, unsigned long val, void *data)
-{
-	struct task_struct *task = data;
-
-	if (task == lowmem_deathpending) {
-		lowmem_deathpending = NULL;
-		lowmem_print(2, "deathpending end %d (%s)\n",
-			task->pid, task->comm);
-	}
-
-	return NOTIFY_OK;
-}
-
-static void dump_deathpending(struct task_struct *t_deathpending)
-{
-	struct task_struct *p;
-
-	if (lowmem_debug_level < DEBUG_LEVEL_DEATHPENDING)
-		return;
-
-	BUG_ON(!t_deathpending);
-	lowmem_print(DEBUG_LEVEL_DEATHPENDING, "deathpending %d (%s)\n",
-		t_deathpending->pid, t_deathpending->comm);
-
-	read_lock(&tasklist_lock);
-	for_each_process(p) {
-		struct mm_struct *mm;
-		struct signal_struct *sig;
-		int oom_adj;
-		int tasksize;
-
-		task_lock(p);
-		mm = p->mm;
-		sig = p->signal;
-		if (!mm || !sig) {
-			task_unlock(p);
-			continue;
-		}
-		oom_adj = sig->oom_adj;
-		tasksize = get_mm_rss(mm);
-		task_unlock(p);
-		lowmem_print(DEBUG_LEVEL_DEATHPENDING,
-			"  %d (%s), adj %d, size %d\n",
-			p->pid, p->comm,
-			oom_adj, tasksize);
-	}
-	read_unlock(&tasklist_lock);
-}
 
 #ifdef CONFIG_MEMORY_HOTPLUG
 static int lmk_hotplug_callback(struct notifier_block *self,
@@ -198,18 +140,6 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 			}
 		}
 	}
-	/*
-	 * If we already have a death outstanding, then
-	 * bail out right away; indicating to vmscan
-	 * that we have nothing further to offer on
-	 * this pass.
-	 *
-	 */
-	if (lowmem_deathpending &&
-	    time_before_eq(jiffies, lowmem_deathpending_timeout)) {
-		dump_deathpending(lowmem_deathpending);
-		return 0;
-	}
 
 	if (lowmem_adj_size < array_size)
 		array_size = lowmem_adj_size;
@@ -253,6 +183,12 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		if (!p)
 			continue;
 
+		if (test_tsk_thread_flag(p, TIF_MEMDIE) &&
+		    time_before_eq(jiffies, lowmem_deathpending_timeout)) {
+			task_unlock(p);
+			rcu_read_unlock();
+			return 0;
+		}
 		oom_score_adj = p->signal->oom_score_adj;
 		if (oom_score_adj < min_score_adj) {
 			task_unlock(p);
@@ -279,9 +215,9 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		lowmem_print(1, "send sigkill to %d (%s), adj %d, size %d\n",
 			     selected->pid, selected->comm,
 			     selected_oom_score_adj, selected_tasksize);
-		lowmem_deathpending = selected;
 		lowmem_deathpending_timeout = jiffies + HZ;
 		send_sig(SIGKILL, selected, 0);
+		set_tsk_thread_flag(selected, TIF_MEMDIE);
 		rem -= selected_tasksize;
 	}
 	lowmem_print(4, "lowmem_shrink %lu, %x, return %d\n",
@@ -297,7 +233,6 @@ static struct shrinker lowmem_shrinker = {
 
 static int __init lowmem_init(void)
 {
-	task_free_register(&task_nb);
 	register_shrinker(&lowmem_shrinker);
 #ifdef CONFIG_MEMORY_HOTPLUG
 	hotplug_memory_notifier(lmk_hotplug_callback, 0);
@@ -308,7 +243,6 @@ static int __init lowmem_init(void)
 static void __exit lowmem_exit(void)
 {
 	unregister_shrinker(&lowmem_shrinker);
-	task_free_unregister(&task_nb);
 }
 
 module_param_named(cost, lowmem_shrinker.seeks, int, S_IRUGO | S_IWUSR);
