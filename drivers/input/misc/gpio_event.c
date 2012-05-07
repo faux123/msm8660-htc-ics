00ae807685d2cceac49ece57722eb314d94b6cde
faux123 <reioux@gmail.com>
2012-05-07 13:46:13 -0700

    misc/gpio_event: add HTC Holiday fix up

--- drivers/input/misc/gpio_event.c
+++ drivers/input/misc/gpio_event.c
@@ -170,7 +170,12 @@ static int gpio_event_probe(struct platform_device *pdev)
 		input_dev->event = gpio_input_event;
 		ip->input_devs->dev[i] = input_dev;
 #ifdef CONFIG_TOUCHSCREEN_CYPRESS_SWEEP2WAKE
+#if defined (CONFIG_MACH_PYRAMID)
 		if (!strcmp(input_dev->name, "pyramid-keypad")) {
+#endif
+#if defined (CONFIG_MACH_HOLIDAY)
+		if (!strcmp(input_dev->name, "holiday-keypad")) {
+#endif
 			sweep2wake_setdev(input_dev);
 			printk(KERN_INFO "[sweep2wake]: set device %s\n", input_dev->name);
 		}
