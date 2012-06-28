c7ccd94b2ae8fbabcbcee25521cdc1843038537c
Yank555-lu <yank555.lu@gmail.com>
2012-06-28 02:21:22 +0200

    USB forced fast charge v2
    Soft force mode has been added (= mode 2).
    
    In this mode fast charge will only kick in if no USB device is detected (as should be the case for wall/car chargers). Please note that in this case fast charge will NOT work on a USB 2.0 and 3.0 port on a computer !
    
    Use sysfs interface to change behaviour :
    
     * /sys/kernel/fast_charge/force_fast_charge (rw)
     *
     *   0 - disabled (default)
     *   1 - substitute AC to USB charging always
     *   2 - substitute AC to USB charging only if no USB peripheral is detected
    
    For informational purposes I've also added a read-only sysfs file that indicates the current USB state :
    
     * /sys/kernel/fast_charge/USB_peripheral_detected (ro)
     *
     * 0 - No USB device currently attached
     * 1 - USB device currently attached
    
    Consider this beta, it needs testing !

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -43,6 +43,10 @@
 #include <mach/clk.h>
 #include <mach/cable_detect.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
 #define MSM_USB_BASE	(motg->regs)
 #define DRIVER_NAME	"msm_otg"
 
@@ -60,6 +64,15 @@ static void send_usb_connect_notify(struct work_struct *w)
 
 	motg->connect_type_ready = 1;
 	USBH_INFO("send connect type %d\n", motg->connect_type);
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (motg->connect_type == CONNECT_TYPE_USB) {
+		USB_peripheral_detected = USB_ACC_DETECTED; /* Inform forced fast charge that a USB accessory has been attached */
+		USBH_INFO("USB forced fast charge : USB device currently attached");
+	} else {
+		USB_peripheral_detected = USB_ACC_NOT_DETECTED; /* Inform forced fast charge that a USB accessory has not been attached */
+		USBH_INFO("USB forced fast charge : No USB device currently attached");
+	}
+#endif
 	mutex_lock(&notify_sem);
 	list_for_each_entry(notifier, &g_lh_usb_notifier_list, notifier_link) {
 		if (notifier->func != NULL) {
