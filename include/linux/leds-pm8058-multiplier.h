b1c43c2dd1da4a2d45b79ff3f21fafbb8e3bd373
Yank555-lu <yank555.lu@gmail.com>
2012-07-30 21:46:45 +0200

    Notification LED duration mod (& Kernel v1.8 config)
    Added entry in menuconfig to optionally add the mod to the kernel compile.
    
    Use sysfs interface to change behaviour :
    
     * /sys/kernel/notification_leds/off_timer_multiplier (rw)
     *
     *   0 - Notification LED will blink forever until cleared by user
     *   1 - Notification LED will blink as requested by app or until cleared by user (default)
     *   2 to 255 - Notification LED will blink as requested by app multiplied by value or until cleared by user
    
    NB: The output of the sysfs file (e.g. if you 'cat' it) will be clear text.

---
+++ include/linux/leds-pm8058-multiplier.h
@@ -0,0 +1,24 @@
+/*
+ * Author: Chad Froebel <chadfroebel@gmail.com>
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _LINUX_LED_OFF_TMR_H
+
+#define _LINUX_LED_OFF_TMR_H
+
+extern int off_timer_multiplier;
+
+#define OFF_TIMER_INFINITE 0
+#define OFF_TIMER_NORMAL 1
+
+#endif
