c5ef8d01183b7500355b10a0ca256f0b53c0a84a
yank555-lu <yank555.lu@gmail.com>
2012-08-18 16:43:28 +0200

    Corrected compiled in CPU frequencies (& Yank555.lu 2.21 config) :
    CPU_max= 1188000
    Intelledemand_active_max = 1188000
    Intelledemand_inactive_max = 918000
    Intelledemand_two_phase = 810000
    
    According to faux's directive : 2 phase < inactive max < active_max == scaling max

--- arch/arm/mach-msm/board-pyramid.c
+++ arch/arm/mach-msm/board-pyramid.c
@@ -6220,9 +6220,9 @@ static void __init msm8x60_init(struct msm_board_data *board_data)
 #endif
 
 #ifdef CONFIG_CPU_FREQ_GOV_ONDEMAND_2_PHASE
-	set_two_phase_freq(918000);
+	set_two_phase_freq(810000);
 #ifdef CONFIG_CPU_FREQ_GOV_INTELLIDEMAND
-	id_set_two_phase_freq(918000);
+	id_set_two_phase_freq(810000);
 #endif
 #endif
 
