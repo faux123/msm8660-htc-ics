7c967e288182c51cb9f9936abe162bd934572490
faux123 <reioux@gmail.com>
2012-09-18 19:49:30 -0500

    gpu overclock: add 300MHz entry and change max GPU to 300MHz
    also adjust the low nominal entry from 178MHz to 160MHz to further
    reduce gpu clock

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -932,7 +932,7 @@ static struct resource kgsl_3d0_resources[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 320000000,
+			.gpu_freq = 300000000,
 			.bus_freq = 4,
 			.io_fraction = 0,
 		},
@@ -947,7 +947,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.io_fraction = 100,
 		},
 		{
-			.gpu_freq = 177778000,
+			.gpu_freq = 160000000,
 			.bus_freq = 1,
 			.io_fraction = 100,
 		},
